5d027eb7c0844f7451c486b737197643a71928a7
EgorKluch <EgorKluch@gmail.com>
2016-08-17 13:04:41 +0300

    Add index.html. Simplest app works

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: "./src/js/app.js",
   output: {
-    path: __dirname + '/build/js',
-    filename: "index.js"
+    path: __dirname + '/build',
+    filename: "js/app.js"
   },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: 'src/index.html', to: 'index.html'
+    }])
+  ],
   module: {
     loaders: []
   }
