c44597f63d069f9add693e2d15b7184ed386916f
EgorKluch <EgorKluch@gmail.com>
2016-08-17 18:04:57 +0300

    Add Marionette

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+var $ = require('jquery');
+var Marionette = require('backbone.marionette');
+
+$(document).ready(function () {
+  var app = new Marionette.Application();
+
+  app.on('start', function() {
+    app.addRegions({
+      app: "#app"
+    });
+  });
+
+  app.start();
+});
